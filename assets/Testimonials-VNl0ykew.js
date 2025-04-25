e7c4c478b72dd06a2fced816b9fc917813dab286
Sarwan Yadav <sarwanyadav6174@gmail.com>
2025-04-25 21:52:08 +0530

    Updates

--- assets/Testimonials-VNl0ykew.js
+++ assets/Testimonials-VNl0ykew.js
@@ -1,4 +1,4 @@
-import{h as w,r,j as e}from"./index-doU7BU81.js";import{u as q}from"./use-scroll-animation-BdN_jbjl.js";import{M as A,a as E,P as T,b as $,c as G,L as W,m as D}from"./NavBar-B4PbPpmH.js";/**
+import{h as w,r,j as e}from"./index-B8jl8hVv.js";import{u as q}from"./use-scroll-animation-x4zoTyzP.js";import{M as A,a as E,P as T,b as $,c as G,L as W,m as D}from"./NavBar-C7BxXWKy.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
